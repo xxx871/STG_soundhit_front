183dd93a85b8380c2c93dc3b6c3931ae95474609
xxx871 <0kh38c317264.7u@gmail.com>
2024-06-11 16:44:27 +0900

    プロフィール編集機能実装

--- types/interface.ts
+++ types/interface.ts
@@ -4,3 +4,22 @@ export interface Score {
   difficulty: string;
   score: number;
 };
+
+export interface User {
+  name: string;
+  gender: string | null;
+  user_high_note: Note | null;
+  user_low_note: Note | null;
+};
+
+export interface Gender {
+  id: number;
+  name: string;
+};
+
+export interface Note {
+  id: number;
+  frequency: number;
+  ja_note_name: string;
+  en_note_name: string;
+};
