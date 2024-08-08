2570a75766c82de406c9fb121371ff926310248b
xxx871 <0kh38c317264.7u@gmail.com>
2024-08-08 15:54:46 +0900

    遊び方モーダルの更新

--- app/components/layouts/Header/Header.tsx
+++ app/components/layouts/Header/Header.tsx
@@ -2,15 +2,15 @@ import LogoutButton from '@/features/auth/components/LogoutButton';
 import { getUserSession } from '@/lib/session';
 import Link from 'next/link'
 import React from 'react'
-import ModalTutorial from './ModalTutorial';
 import Image from 'next/image';
+import ModalTutorial from '@/features/tutorial/components/ModalTutorial';
 
 const Header = async () => {
   const userSession = await getUserSession();
   const isLoggedIn = userSession ? userSession.is_login : false;
 
   return (
-    <div className="divide-y border-gray-200 dark:border-gray-800 border-b bg-inherit h-12 flex items-center">
+    <header className="divide-y border-gray-200 dark:border-gray-800 border-b bg-inherit h-12 flex items-center">
       <div className="px-4 md:py-2 lg:px-6 w-full">
         <div className="flex items-center justify-between space-y-2 md:space-y-0 md:space-x-6 text-white">
           <Link href="/" className="text-2xl font-bold font-palettemosaic flex items-center">
@@ -22,14 +22,14 @@ const Header = async () => {
             />
             おんぴしゃ
           </Link>
-          <nav className="flex justify-end items-center space-x-6 text-2xl font-medium text-white">
+          <nav className="flex justify-end items-center text-2xl font-medium text-white">
             <ModalTutorial />
-            <Link href="/rankings" className="transition-colors hover:text-gray-300">
+            <Link href="/rankings" className="transition-colors hover:text-gray-300 mx-6">
               ランキング
             </Link>
             {isLoggedIn ? (
               <>
-                <Link href="/profile">プロフィール</Link>
+                <Link href="/profile" className="transition-colors hover:text-gray-300 mr-6">プロフィール</Link>
                 <LogoutButton
                   type="button"
                 >
@@ -37,14 +37,14 @@ const Header = async () => {
                 </LogoutButton>
               </>
             ) : (
-              <Link href="/login" className="bg-black py-1 px-3 rounded-md font-medium text-xl">
+              <Link href="/login" className="bg-black text-white py-1 px-3 rounded-md font-medium text-xl transition-colors hover:bg-white hover:text-black">
                 ログイン
               </Link>
             )}
           </nav>
         </div>
       </div>
-    </div>
+    </header>
   )
 }
 
